ee983443bb6bf9630ee1d2891a3f46b440205af5
Mikhail_Krasko <97303138+Krasko25@users.noreply.github.com>
2024-10-15 13:41:08 +0300

    Добавил решения для оставшихся заданий

---
+++ Base_Programmin_1/ЛР2/Индивидуальное задание 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("\n\nИндивидуальное задание 2\n\n");
+        
+        Console.WriteLine("Трехзначные числа, у которых первая и третья цифры совпадают:");
+
+        // Перебор всех трехзначных чисел
+        for (int number = 100; number <= 999; number++)
+        {
+            int firstDigit = number / 100;      // Первая цифра
+            int thirdDigit = number % 10;       // Третья цифра
+
+            // Проверка на совпадение
+            if (firstDigit == thirdDigit)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
+
