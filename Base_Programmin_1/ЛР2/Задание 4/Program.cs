0e7eccc1a0f01e2e34e233323d24bc446a066e56
Mikhail <97303138+Krasko25@users.noreply.github.com>
2024-10-15 12:03:54 +0300

    Добавил решения задание 3, 4, 5

---
+++ Base_Programmin_1/ЛР2/Задание 4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("\n\nЗадание 4. Ряд Тейлора\n\n");
+        
+        //Ввод значений
+        Console.Write("x (радианы): ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("q (точность): ");
+        double q = Convert.ToDouble(Console.ReadLine());
+
+        int sign = -1; // Для чередования знаков
+        double factorial = 1;
+        double sum = 1; // Первое слагаемое
+        double temporary = 1; // Текущее слагаемое
+        int n = 0; // Счётчик слагаемых
+
+        while (Math.Abs(temporary) >= q)
+        {
+            n++;
+
+            // Обновляем факториал
+            factorial *= (2 * n - 1) * (2 * n); 
+
+
+            temporary = Math.Pow(x, 2 * n) / factorial;
+            sum += sign * temporary;
+
+            // Меняем знак для следующего слагаемого
+            sign *= -1;
+        }
+
+        // Вывод результата
+        Console.WriteLine($"Примерный cos({x}) = {sum}");
+        Console.WriteLine($"Количество учтенных слагаемых: {n}");
+    }
+}
+
---
+++ Base_Programmin_1/ЛР2/Задание 5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("\n\nЗадание 5. Комбинации чисел\n\n");
+        
+        int N;
+
+        // Проверка N на натуральность
+        while (true){
+            Console.Write("Введите натуральное число N: ");
+            N = Convert.ToInt32(Console.ReadLine());
+            if (N > 0){
+                break; // Если число натуральное, выходим из цикла
+            }
+            else{
+                Console.WriteLine("Вы должны были ввести " +
+                "положительное целое число.");
+            }
+        }
+
+        bool found = false; // Флаг для определения, найдена ли хотя бы одна комбинация
+
+        // Перебираем все комбинации
+        for (int x = 1; x * x * x <= N; x++){
+            for (int y = 1; y * y * y <= N; y++){
+                for (int z = 1; z * z * z <= N; z++){
+                    if (x * x * x + y * y * y + z * z * z == N){
+                        Console.WriteLine($"x = {x}, y = {y}, z = {z}");
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        // Если не найдена ни одна комбинация
+        if (!found){
+            Console.WriteLine("No such combinations!");
+        }
+    }
+}
+
