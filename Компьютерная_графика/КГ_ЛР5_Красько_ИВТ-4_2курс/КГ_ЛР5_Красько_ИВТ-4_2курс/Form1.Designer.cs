4e7b8da0e1b07227889c32ce101bbb29ac2c4fe0
Mikhail <97303138+Krasko25@users.noreply.github.com>
2025-12-09 12:18:41 +0300

    Добавил решение ЛР5

---
+++ Компьютерная_графика/КГ_ЛР5_Красько_ИВТ-4_2курс/КГ_ЛР5_Красько_ИВТ-4_2курс/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace КГ_ЛР5_Красько_ИВТ_4_2курс
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxChart = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxChart
+            // 
+            this.pictureBoxChart.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.pictureBoxChart.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxChart.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBoxChart.Location = new System.Drawing.Point(0, 60);
+            this.pictureBoxChart.Name = "pictureBoxChart";
+            this.pictureBoxChart.Size = new System.Drawing.Size(1192, 775);
+            this.pictureBoxChart.TabIndex = 0;
+            this.pictureBoxChart.TabStop = false;
+            this.pictureBoxChart.Click += new System.EventHandler(this.pictureBoxChart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1192, 835);
+            this.Controls.Add(this.pictureBoxChart);
+            this.Name = "Form1";
+            this.Text = "Оценки студентов";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxChart;
+    }
+}
+
---
+++ Компьютерная_графика/КГ_ЛР5_Красько_ИВТ-4_2курс/КГ_ЛР5_Красько_ИВТ-4_2курс/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("КГ_ЛР5_Красько_ИВТ-4_2курс")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("КГ_ЛР5_Красько_ИВТ-4_2курс")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8cdea93b-8045-44ad-b33d-5cd2c4c9d710")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
