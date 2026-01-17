b8b5e6566eae922d7b4d06607d43f35b9d71a442
Mikhail <97303138+Krasko25@users.noreply.github.com>
2026-01-17 13:53:20 +0300

    Добавил решения заданий 2 и 3, обновил отчёт

---
+++ Base_Programming/Семестр 3/ЛР5/ЛР5_Задание_2_ООП_Красько_2курс_ИВТ-4/ЛР5_Задание_2_ООП_Красько_2курс_ИВТ-4/ЛР5_Задание_2_ООП_Красько_2курс_ИВТ-4.cpp
@@ -0,0 +1,91 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <cstring>
+#include <cctype> // для нижнего регистра
+#include <windows.h>  // Для смены кодировки консоли
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Введите имя файла: ";
+    string filename;
+    cin >> filename;
+
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cout << "Ошибка! Файл не найден." << endl;
+        return 1;
+    }
+
+    map<string, int> wordsMap;
+
+    const size_t stringLen = 250;
+    char line[stringLen];
+
+    while (file.getline(line, stringLen)) {
+        char* word = strtok(line, " .,-:!;");
+
+        while (word != NULL) {
+            string currentWord = word;
+
+            // Всё слова должны быть в нижнем регистре
+            for (size_t i = 0; i < currentWord.length(); i++) {
+                currentWord[i] = tolower((unsigned char)currentWord[i]);
+            }
+
+            // Проверяем длину слова
+            if (currentWord.length() > 3) {
+                wordsMap[currentWord]++;
+            }
+            // следующее слово для следующей иттерации
+            word = strtok(NULL, " .,-:!;");
+        }
+    }
+
+    file.close();
+
+    // Переносим в вектор пар сторок с количеством вхождений
+    vector<pair<string, int>> wordsVector;
+
+    for (auto it = wordsMap.begin(); it != wordsMap.end(); ++it) {
+        if (it->second >= 7) {
+            wordsVector.push_back(make_pair(it->first, it->second));
+        }
+    }
+
+    // пузырьковая сортировка
+    if (!wordsVector.empty()) {
+        for (size_t i = 0; i < wordsVector.size(); i++) {
+            for (size_t j = 0; j < wordsVector.size() - 1; j++) {
+                if (wordsVector[j].second < wordsVector[j + 1].second) {
+                    auto temp = wordsVector[j];
+                    wordsVector[j] = wordsVector[j + 1];
+                    wordsVector[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    // Выводим результаты
+    cout << "\nСлова длиной > 3, которые встречаются > 6 раз):\n-----------------------------------------\n";
+
+    if (wordsVector.empty()) {
+        cout << "Нет слов, удовлетворяющих условию" << endl;
+    }
+    else {
+        for (size_t i = 0; i < wordsVector.size(); i++) {
+            cout << wordsVector[i].first << "\t\t" << wordsVector[i].second << endl;
+        }
+    }
+
+    return 0;
+}
---
+++ Base_Programming/Семестр 3/ЛР5/ЛР5_Задание_3_ООП_Красько_2курс_ИВТ-4/ЛР5_Задание_3_ООП_Красько_2курс_ИВТ-4/ЛР5_Задание_3_ООП_Красько_2курс_ИВТ-4.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+class Book {
+private:
+    string name;  
+    string author;
+    int year;
+
+public:
+    Book(string n, string a, int y) {
+        name = n;
+        author = a;
+        year = y;
+    }
+
+    // Геттеры
+    string getName() const {
+        return name;
+    }
+
+    string getAuthor() const {
+        return author;
+    }
+
+    int getYear() const {
+        return year;
+    }
+};
+
+// Функтор для сортировки
+class BookSorter {
+public:
+    bool operator()(Book* b1, Book* b2) {
+        // В первую очередь сравниваются авторы
+        if (b1->getAuthor() != b2->getAuthor()) {
+            return b1->getAuthor() < b2->getAuthor();
+        }
+        // Если авторы одинаковые, сравниваются названия
+        return b1->getName() < b2->getName();
+    }
+};
+
+// Функтор для фильтрации книг по годам
+class BookFinder {
+private:
+    int startYear;
+    int endYear;
+
+public:
+    BookFinder(int start, int end) {
+        startYear = start;
+        endYear = end;
+    }
+
+    bool operator()(Book* book) {
+        return book->getYear() >= startYear && book->getYear() <= endYear;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+
+    std::vector<Book*> books;
+    books.push_back(new Book("Война и мир", "Толстой Л.Н.", 2010));
+    books.push_back(new Book("Подросток", "Достоевский Ф.М.", 2004));
+    books.push_back(new Book("Обрыв", "Гончаров И.А.", 2010));
+    books.push_back(new Book("Анна Каренина", "Толстой Л.Н.", 1999));
+    books.push_back(new Book("Обыкновенная история", "Гончаров И.А.", 2011));
+    books.push_back(new Book("Утраченные иллюзии", "Бальзак О.", 2009));
+    books.push_back(new Book("Оливер Твист", "Диккенс Ч.", 2001));
+    books.push_back(new Book("Фауст", "Гёте И.В.", 2010));
+    books.push_back(new Book("Лилия долины", "Бальзак О.", 1998));
+
+    std::cout << "\nКниги в алфавитном порядке:\n\n";
+
+    BookSorter bookSorterObject;
+    std::sort(books.begin(), books.end(), bookSorterObject); // bookSorterObject - признак сортировки
+
+    std::vector<Book*>::iterator i; // умный указатель, ходящий по списку книг
+    for (i = books.begin(); i != books.end(); ++i)
+    {
+        std::cout << (*i)->getAuthor() << " \""
+            << (*i)->getName() << "\"" << std::endl;
+    }
+
+    BookFinder bookFinderObject(2005, 2014);
+    std::vector<Book*>::iterator finder = std::find_if(books.begin(), books.end(), bookFinderObject); 
+    // find_if ищет превый элемент, для которого функтор возвращает true
+
+    std::cout << "\nКниги в диапазоне года издания 2005 - 2014:\n\n";
+
+    while (finder != books.end())
+    {
+        std::cout << (*finder)->getAuthor() << " \""
+            << (*finder)->getName() << "\"" << std::endl;
+        finder = std::find_if(++finder, books.end(), bookFinderObject);
+    }
+
+    for (i = books.begin(); i != books.end(); ++i)
+    {
+        delete (*i);
+    }
+
+    return 0;
+}
